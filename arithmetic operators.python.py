a8cd36e99a5b1bfe7934d5f0f0be3a69add49bb6
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2022-11-24 12:49:31 +0530

    Add files via upload

---
+++ arithmetic operators.python.py
@@ -0,0 +1,12 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: C:\Users\kruti\OneDrive\Desktop\python2\simpleinterest.python.py ==
+Enter the value of p9
+Enter the value of r8
+Enter the value of t7
+Simple Interest is: 5.04
+
+============ RESTART: C:\Users\kruti\OneDrive\Desktop\python2\time.python.py ===========
+Enter the value of a6
+time is: 360.0
